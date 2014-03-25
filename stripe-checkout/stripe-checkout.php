4038405ac1be00ef56eb65803213c7ee0f7cb73b
Phil Derksen <pderksen@gmail.com>
2014-03-24 22:26:41 -0700

    Main plugin file organization and clean up. Added plugin blog post and GitHub url.

--- stripe-checkout/stripe-checkout.php
+++ stripe-checkout/stripe-checkout.php
@@ -3,7 +3,7 @@
 /**
  * Simple Stripe Checkout
  *
- * @package   sc
+ * @package   SC
  * @author    Phil Derksen <pderksen@gmail.com>, Nick Young <mycorpweb@gmail.com>
  * @license   GPL-2.0+
  * @link      http://philderksen.com
@@ -11,27 +11,26 @@
  *
  * @wordpress-plugin
  * Plugin Name: Simple Stripe Checkout
- * Plugin URI: http://philderksen.com/
+ * Plugin URI: http://philderksen.com/stripe-checkout-wordpress/
  * Description: Add a simple Stripe Checkout button and overlay to your site using a shortcode.
  * Version: 1.0.0
  * Author: Phil Derksen
  * Author URI: http://philderksen.com
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: sc
- * Domain Path: /languages
+ * GitHub Plugin URI: https://github.com/pderksen/WP-Stripe-Checkout
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
+}
 
-require_once( plugin_dir_path( __FILE__ ) . 'class-stripe-checkout.php' );
-
-// Define constant for this file.
 if ( ! defined( 'SC_MAIN_FILE' ) ) {
 	define( 'SC_MAIN_FILE', __FILE__ );
 }
+
+require_once( plugin_dir_path( __FILE__ ) . 'class-stripe-checkout.php' );
 
 // Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
 register_activation_hook( __FILE__, array( 'Stripe_Checkout', 'activate' ) );
