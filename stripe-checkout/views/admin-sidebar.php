797861256abfd5f14f6be3b73d0b8f1e797edccb
Phil Derksen <pderksen@gmail.com>
2014-03-25 23:21:25 -0700

    Admin sidebar content updates.

--- stripe-checkout/views/admin-sidebar.php
+++ stripe-checkout/views/admin-sidebar.php
@@ -15,12 +15,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="sidebar-container">
-	<h4 class="sidebar-title-medium"><?php _e( 'Test', 'sc' ); ?></h4>
-
 	<div class="sidebar-content">
 		<p>
-			To Do...
+			<?php _e( 'Be the first to know when major new features are released.', 'sc' ); ?>
 		</p>
+
+		<form action="http://philderksen.us1.list-manage1.com/subscribe/post?u=7a94395392f63c258fe90f941&amp;id=e8bf1dff87" method="post" target="_blank" novalidate>
+			<p>
+				<input type="email" name="EMAIL" class="large-text" placeholder="Your email address">
+			</p>
+			<p>
+				<input type="submit" value="<?php _e( 'Keep me in the Loop!', 'sc' ); ?>" name="subscribe" class="button-primary">
+			</p>
+		</form>
 	</div>
 </div>
 
@@ -30,19 +37,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php _e( 'Help us get noticed (and boost our egos) with a rating and short review.', 'sc' ); ?>
 		</p>
 
-		<a href="https://wordpress.org/support/view/plugin-reviews/stripe" class="btn btn-small btn-block btn-inverse" target="_blank">
+		<a href="https://wordpress.org/support/view/plugin-reviews/stripe" class="button-primary" target="_blank">
 			<?php _e( 'Rate this plugin on WordPress.org', 'sc' ); ?></a>
 	</div>
 </div>
 
 <div class="sidebar-container">
 	<div class="sidebar-content">
-		<ul>
-			<li>
-				<i class="fui-arrow-right"></i>
-				<a href="https://wordpress.org/support/plugin/stripe" target="_blank">
-					<?php _e( 'Community Support Forums', 'sc' ); ?></a>
-			</li>
-		</ul>
+		<p>
+			<?php _e( 'Have a feature request or need help from others?', 'sc' ); ?>
+		</p>
+		<p>
+			<a href="https://wordpress.org/support/plugin/stripe" target="_blank">
+				<?php _e( 'Visit our Community Support Forums', 'sc' ); ?></a>
+		</p>
 	</div>
 </div>
