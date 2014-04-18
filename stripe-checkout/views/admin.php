405b29aa8fd9c1cf821ab1d9f56aab6552dd8721
Nick Young <mycorpweb@gmail.com>
2014-04-18 15:51:41 -0600

    Added admin help for new shortcode options

--- stripe-checkout/views/admin.php
+++ stripe-checkout/views/admin.php
@@ -47,28 +47,59 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 			<table class="widefat importers" cellspacing="0">
 				<thead>
-				<tr>
-					<th><?php _e( 'Attribute', 'sc' ); ?></th>
-					<th><?php _e( 'Description', 'sc' ); ?></th>
-					<th><?php _e( 'Default', 'sc' ); ?></th>
-				</tr>
+					<tr>
+						<th><?php _e( 'Attribute', 'sc' ); ?></th>
+						<th><?php _e( 'Description', 'sc' ); ?></th>
+						<th><?php _e( 'Default', 'sc' ); ?></th>
+					</tr>
 				</thead>
 				<tbody>
-				<tr>
-					<td>name</td>
-					<td><?php _e( 'The name of your company or website.', 'sc' ) ?></td>
-					<td>Site Title</td>
-				</tr>
-				<tr>
-					<td>description</td>
-					<td><?php _e( 'A description of the product or service being purchased.', 'sc' ); ?></td>
-					<td><?php _e( 'n/a', 'sc' ); ?></td>
-				</tr>
-				<tr>
-					<td>amount</td>
-					<td><?php _e( 'The amount (in U.S. cents) that\'s shown to the user.', 'sc' ); ?></td>
-					<td>100 (equals $1.00 US)</td>
-				</tr>
+					<tr>
+						<td>name</td>
+						<td><?php _e( 'The name of your company or website.', 'sc' ) ?></td>
+						<td>Site Title</td>
+					</tr>
+					<tr>
+						<td>description</td>
+						<td><?php _e( 'A description of the product or service being purchased.', 'sc' ); ?></td>
+						<td><?php _e( 'n/a', 'sc' ); ?></td>
+					</tr>
+					<tr>
+						<td>amount</td>
+						<td><?php _e( 'The amount (in U.S. cents) that\'s shown to the user.', 'sc' ); ?></td>
+						<td>100 (equals $1.00 US)</td>
+					</tr>
+					
+					<tr>
+						<td>image</td>
+						<td><?php _e( 'A URL pointing to a square image of your brand or product. The recommended minimum size is 128x128px.', 'sc' ); ?></td>
+						<td><?php _e( 'n/a', 'sc' ); ?></td>
+					</tr>
+					<tr>
+						<td>currency</td>
+						<td><?php _e( 'Specify a specific currency by using it\'s 3-letter ISO code.', 'sc' ); ?></td>
+						<td><?php _e( 'USD', 'sc' ); ?></td>
+					</tr>
+					<tr>
+						<td>checkout_button_label</td>
+						<td><?php _e( 'The label of the payment button in the Checkout form. You can use {{amount}} to display the amount.', 'sc' ); ?></td>
+						<td><?php _e( 'Pay {{amount}}', 'sc' ); ?></td>
+					</tr>
+					<tr>
+						<td>billing</td>
+						<td><?php _e( 'Used to gather the billing address during the checkout process.', 'sc' ); ?></td>
+						<td><?php _e( 'false', 'sc' ); ?></td>
+					</tr>
+					<tr>
+						<td>shipping</td>
+						<td><?php _e( 'Used to gather the shipping address during the checkout process.', 'sc' ); ?></td>
+						<td><?php _e( 'false', 'sc' ); ?></td>
+					</tr>
+					<tr>
+						<td>enable_remember</td>
+						<td><?php _e( 'Adds a "remember me" checkbox to the checkout form.', 'sc' ); ?></td>
+						<td><?php _e( 'true', 'sc' ); ?></td>
+					</tr>
 				</tbody>
 			</table>
 
