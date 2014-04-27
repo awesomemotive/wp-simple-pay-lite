836e96f3a37c97c52c87da835ad1ac89cd4de32c
Nick Young <mycorpweb@gmail.com>
2014-04-26 23:50:37 -0600

    Added in functionality of desault settings options into shortcodes

--- stripe-checkout/includes/shortcodes.php
+++ stripe-checkout/includes/shortcodes.php
@@ -21,19 +21,18 @@ function sc_stripe_shortcode( $attr ) {
 	global $sc_options;
 	
 	extract( shortcode_atts( array(
-					'name'                  => get_bloginfo( 'title' ),
-					'description'           => '',
-					'amount'                => '',
-					'image_url'             => '',
-					'currency'              => 'USD',
-					'checkout_button_label' => '',
-					'billing'               => '',    // true or false
-					'shipping'              => '',    // true or false
-					'payment_button_label'  => '',
-					'enable_remember'       => '',    // true or false
-					'success_redirect_url'  => get_permalink()
+					'name'                  => ( ! empty( $sc_options['name'] )                  ? $sc_options['name']                  : get_bloginfo( 'title' ) ),
+					'description'           => ( ! empty( $sc_options['description'] )           ? $sc_options['description']           : '' ),
+					'amount'                => ( ! empty( $sc_options['amount'] )                ? $sc_options['amount']                : '' ),
+					'image_url'             => ( ! empty( $sc_options['image_url'] )             ? $sc_options['image_url']             : '' ),
+					'currency'              => ( ! empty( $sc_options['currency'] )              ? $sc_options['currency']              : 'USD' ),
+					'checkout_button_label' => ( ! empty( $sc_options['checkout_button_label'] ) ? $sc_options['checkout_button_label'] : '' ),
+					'billing'               => ( ! empty( $sc_options['billing'] )               ? $sc_options['billing']               : '' ),    // true or false
+					'shipping'              => ( ! empty( $sc_options['shipping'] )              ? $sc_options['shipping']              : '' ),    // true or false
+					'payment_button_label'  => ( ! empty( $sc_options['payment_button_label'] )  ? $sc_options['payment_button_label']  : '' ),
+					'enable_remember'       => ( ! empty( $sc_options['enable_remember'] )       ? $sc_options['enable_remember']       : 0 ),    // true or false
+					'success_redirect_url'  => ( ! empty( $sc_options['success_redirect_url'] )  ? $sc_options['success_redirect_url']  : get_permalink() )
 				), $attr ) );
-	
 	
 	if( empty( $amount ) || $amount < 50 )
 		return '';
@@ -64,8 +63,7 @@ function sc_stripe_shortcode( $attr ) {
 	$script_options .= ( ( ! empty( $billing ) && $billing != 'false' ) ? 'data-billing-address="' . esc_attr( $billing ) . '" ' : '' );
 	$script_options .= ( ( ! empty( $shipping ) && $shipping != 'false' ) ? 'data-shipping-address="' . esc_attr( $shipping ) . '" ' : '' );
 	$script_options .= ( ! empty( $payment_button_label ) ? 'data-label="' . esc_attr( $payment_button_label ) . '" ' : '' );
-	$script_options .= ( ! empty( $enable_remember ) ? 'data-allow-remember-me="' . esc_attr( $enable_remember ) . '" ' : '' );
-	
+	$script_options .= ( $enable_remember != 1 ? 'data-allow-remember-me="false" ' : '' );
 	
 	
 	if( ! isset( $_GET['payment'] ) ) { 
