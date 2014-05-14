b19a1243a265a4cb69e13e83ec9662993aba39e7
Nick Young <mycorpweb@gmail.com>
2014-05-14 12:57:57 -0600

    Updated checks for test/live keys

--- stripe-checkout/includes/misc-functions.php
+++ stripe-checkout/includes/misc-functions.php
@@ -35,7 +35,7 @@ function sc_charge_card() {
 		$name        = $_POST['sc-name'];
 		$currency    = $_POST['sc-currency'];
 		
-		if( empty( $sc_options['enable_test_key'] ) ) {
+		if( ! empty( $sc_options['enable_live_key'] ) ) {
 			$key = ( ! empty( $sc_options['live_secret_key'] ) ? $sc_options['live_secret_key'] : '' );
 		} else {
 			$key = ( ! empty( $sc_options['test_secret_key'] ) ? $sc_options['test_secret_key'] : '' );
