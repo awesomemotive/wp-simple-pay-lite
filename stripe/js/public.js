4ceef24f61443ab78fe266668b63f186abede483
Nick Young <mycorpweb@gmail.com>
2014-05-27 02:21:07 -0600

    Early stages of JS rewrite

--- stripe/js/public.js
+++ stripe/js/public.js
@@ -1,71 +1,116 @@
 (function ($) {
 	"use strict";
 	$(function () {
+		
+		
+		// TODO
+		// Pretty sure we don't need to set the handler variables as global since we usually just modify
+		// the sc_script variables. As long as we set the current form and dataAttr globally I think we will be okay
+		window.stripeCheckout = {
+			// Handler variables
+			/*
+			key:             sc_script,
+			image:           '',
+			tokenFunctions:  new Array(),
+			name:            '',
+			description:     '',
+			amount:          '',
+			currency:        '',
+			panelLabel:      '',
+			billingAddress:  '',
+			shippingAddress: '',
+			allowRememberMe: '',
+			email:           '',
+			*/
+			// Functions to run
+			functions:       new Array(),
+			// Form Details
+			currentForm:     '',
+			dataAttr:        ''
+			// Validation functions
+			//validateFunctions: new Array()
+		};
+		
+		//stripeCheckout.functions.push( function() {
+			//alert( 'Hello World #1' );
+		//});
+		
+		
+		//alert( stripeHandlerParams.key );
 			/*
 			 * Call on click handler when button is clicked then use amount (need to sanitize first) and pass to the 
 			 * Stripe Checkout handler
 			 */	
 			$( 'button.sc_checkout' ).closest( 'form' ).submit( function( event ) {
+				
 
+				
 				// To proceed with form submit.
-				var currentForm = $( this );
+				//var currentForm = $( this );
+				
+				stripeCheckout.currentForm = $(this);
 				
 				event.preventDefault();
 				
-				var dataAttr = $(this).attr( 'data-sc-id' );
+				//var dataAttr = $(this).attr( 'data-sc-id' );
+				
+				stripeCheckout.dataAttr = $(this).attr('data-sc-id');
+				
+				
+				/*for( var i = 0; i < stripeCheckout.validateFunctions.length; i++ ) {
+					if( stripeCheckout.validateFunctions[i]() == false ) {
+						return false;
+					}
+				}*/
+				
+				
+				for( var i = 0; i < stripeCheckout.functions.length; i++ ) {
+					stripeCheckout.functions[i]();
+				}
+				
+				//return false;
 				
 				var handler = StripeCheckout.configure({
-					key: sc_script[dataAttr].key,
-					image: ( sc_script[dataAttr].image != -1 ? sc_script[dataAttr].image : '' ),
+					key: sc_script[stripeCheckout.dataAttr].key,
+					image: ( sc_script[stripeCheckout.dataAttr].image != -1 ? sc_script[stripeCheckout.dataAttr].image : '' ),
 					token: function(token, args) {
 					  
 						// Set the values on our hidden elements to pass when submitting the form for payment
-						currentForm.find('.sc_stripeToken').val( token.id );
-						currentForm.find('.sc_amount').val( sc_script[dataAttr].amount );
-						currentForm.find('.sc_stripeEmail').val( token.email );
+						stripeCheckout.currentForm.find('.sc_stripeToken').val( token.id );
+						stripeCheckout.currentForm.find('.sc_amount').val( sc_script[stripeCheckout.dataAttr].amount );
+						stripeCheckout.currentForm.find('.sc_stripeEmail').val( token.email );
 
 						// Add shipping fields values if the shipping information is filled
 						if( ! $.isEmptyObject( args ) ) {
-							currentForm.find('.sc-shipping-name').val(args.shipping_name);
-							currentForm.find('.sc-shipping-country').val(args.shipping_address_country);
-							currentForm.find('.sc-shipping-zip').val(args.shipping_address_zip);
-							currentForm.find('.sc-shipping-state').val(args.shipping_address_state);
-							currentForm.find('.sc-shipping-address').val(args.shipping_address_line1);
-							currentForm.find('.sc-shipping-city').val(args.shipping_address_city);
+							stripeCheckout.currentForm.find('.sc-shipping-name').val(args.shipping_name);
+							stripeCheckout.currentForm.find('.sc-shipping-country').val(args.shipping_address_country);
+							stripeCheckout.currentForm.find('.sc-shipping-zip').val(args.shipping_address_zip);
+							stripeCheckout.currentForm.find('.sc-shipping-state').val(args.shipping_address_state);
+							stripeCheckout.currentForm.find('.sc-shipping-address').val(args.shipping_address_line1);
+							stripeCheckout.currentForm.find('.sc-shipping-city').val(args.shipping_address_city);
 						}
 						
 						//Unbind right before submitting so we don't get stuck in a loop
-						currentForm.unbind('submit');
+						stripeCheckout.currentForm.unbind('submit');
 
-						currentForm.submit();
+						stripeCheckout.currentForm.submit();
 					}
 				 });
 				 
 				 handler.open({
-					 name: ( sc_script[dataAttr].name != -1 ? sc_script[dataAttr].name : '' ),
-					 description: ( sc_script[dataAttr].description != -1 ? sc_script[dataAttr].description : '' ),
-					 amount: sc_script[dataAttr].amount,
-					 currency: ( sc_script[dataAttr].currency != -1 ? sc_script[dataAttr].currency : 'USD' ),
-					 panelLabel: ( sc_script[dataAttr].panelLabel != -1 ? sc_script[dataAttr].panelLabel : 'Pay {{amount}}' ),
-					 billingAddress: ( sc_script[dataAttr].billingAddress == 'true' || sc_script[dataAttr].billingAddress == 1 ? true : false ),
-					 shippingAddress: ( sc_script[dataAttr].shippingAddress == 'true' || sc_script[dataAttr].shippingAddress == 1 ? true : false ),
-					 allowRememberMe: ( sc_script[dataAttr].allowRememberMe == 1 || sc_script[dataAttr].allowRememberMe == 'true' ?  true : false ),
-					 email: ( sc_script[dataAttr].email != -1 ?  sc_script[dataAttr].email : '' )
+					 name: ( sc_script[stripeCheckout.dataAttr].name != -1 ? sc_script[stripeCheckout.dataAttr].name : '' ),
+					 description: ( sc_script[stripeCheckout.dataAttr].description != -1 ? sc_script[stripeCheckout.dataAttr].description : '' ),
+					 amount: sc_script[stripeCheckout.dataAttr].amount,
+					 currency: ( sc_script[stripeCheckout.dataAttr].currency != -1 ? sc_script[stripeCheckout.dataAttr].currency : 'USD' ),
+					 panelLabel: ( sc_script[stripeCheckout.dataAttr].panelLabel != -1 ? sc_script[stripeCheckout.dataAttr].panelLabel : 'Pay {{amount}}' ),
+					 billingAddress: ( sc_script[stripeCheckout.dataAttr].billingAddress == 'true' || sc_script[stripeCheckout.dataAttr].billingAddress == 1 ? true : false ),
+					 shippingAddress: ( sc_script[stripeCheckout.dataAttr].shippingAddress == 'true' || sc_script[stripeCheckout.dataAttr].shippingAddress == 1 ? true : false ),
+					 allowRememberMe: ( sc_script[stripeCheckout.dataAttr].allowRememberMe == 1 || sc_script[stripeCheckout.dataAttr].allowRememberMe == 'true' ?  true : false ),
+					 email: ( sc_script[stripeCheckout.dataAttr].email != -1 ?  sc_script[stripeCheckout.dataAttr].email : '' )
 				 });
 				 
 				 event.preventDefault();
 
 			});
-			
-			function convert_amount( amount, currency ) {
-	
-				var zero_based = new Array( 'BIF', 'CLP', 'DJF', 'GNF', 'JPY', 'KMF', 'KRW', 'MGA', 'PYG', 'RWF', 'VUV', 'XAF', 'XOF', 'XPF' );
-
-				if( $.inArray(currency, zero_based) != -1 ) {
-					return amount;
-				}
-
-				return amount * 100;
-			}	
 	});
 }(jQuery));
