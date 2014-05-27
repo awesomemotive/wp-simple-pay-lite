3108f1f9ff91a41e932e61ae8abbb24b79da0cbf
Nick Young <mycorpweb@gmail.com>
2014-05-26 20:47:01 -0600

    Update Stripe library to 1.14.1

--- stripe/stripe-php/Stripe/Object.php
+++ stripe/stripe-php/Stripe/Object.php
@@ -34,15 +34,17 @@ class Stripe_Object implements ArrayAccess
 
     $this->_retrieveOptions = array();
     if (is_array($id)) {
-      foreach ($id as $key => $value) {
-        if ($key != 'id')
+      foreach($id as $key => $value) {
+        if ($key != 'id') {
           $this->_retrieveOptions[$key] = $value;
+        }
       }
       $id = $id['id'];
     }
 
-    if ($id)
+    if ($id !== null) {
       $this->id = $id;
+    }
   }
 
   // Standard accessor magic methods
@@ -165,10 +167,11 @@ class Stripe_Object implements ArrayAccess
     // Wipe old state before setting new.  This is useful for e.g. updating a
     // customer, where there is no persistent card parameter.  Mark those values
     // which don't persist as transient
-    if ($partial)
+    if ($partial) {
       $removed = new Stripe_Util_Set();
-    else
+    } else {
       $removed = array_diff(array_keys($this->_values), array_keys($values));
+    }
 
     foreach ($removed as $k) {
       if (self::$permanentAttributes->includes($k))
@@ -180,10 +183,11 @@ class Stripe_Object implements ArrayAccess
       if (self::$permanentAttributes->includes($k) && isset($this[$k]))
         continue;
 
-      if (self::$nestedUpdatableAttributes->includes($k) && is_array($v))
+      if (self::$nestedUpdatableAttributes->includes($k) && is_array($v)) {
         $this->_values[$k] = Stripe_Object::scopedConstructFrom('Stripe_AttachedObject', $v, $apiKey);
-      else
+      } else {
         $this->_values[$k] = Stripe_Util::convertToStripeObject($v, $apiKey);
+      }
 
       $this->_transientValues->discard($k);
       $this->_unsavedValues->discard($k);
@@ -231,10 +235,11 @@ class Stripe_Object implements ArrayAccess
 
   public function __toJSON()
   {
-    if (defined('JSON_PRETTY_PRINT'))
+    if (defined('JSON_PRETTY_PRINT')) {
       return json_encode($this->__toArray(true), JSON_PRETTY_PRINT);
-    else
+    } else {
       return json_encode($this->__toArray(true));
+    }
   }
 
   public function __toString()
@@ -244,10 +249,11 @@ class Stripe_Object implements ArrayAccess
 
   public function __toArray($recursive=false)
   {
-    if ($recursive)
+    if ($recursive) {
       return Stripe_Util::convertStripeObjectToArray($this->_values);
-    else
+    } else {
       return $this->_values;
+    }
   }
 }
 
