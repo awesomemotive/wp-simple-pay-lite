8b0de0c2233d12807e61968a1c97937e0fe52f72
Nick Young <mycorpweb@gmail.com>
2014-06-02 10:28:41 -0600

    Added check to see if secret key is empty before outputting the button

--- stripe/includes/shortcodes.php
+++ stripe/includes/shortcodes.php
@@ -41,8 +41,16 @@ function sc_stripe_shortcode( $attr, $content = null ) {
 	// Check if in test mode or live mode
 	if( ! empty( $sc_options['enable_live_key'] ) && $sc_options['enable_live_key'] == 1 ) {
 		$data_key = ( ! empty( $sc_options['live_publish_key'] ) ? $sc_options['live_publish_key'] : '' );
+		
+		if( empty( $sc_options['live_secret_key'] ) ) {
+			$data_key = '';
+		}
 	} else {
 		$data_key = ( ! empty( $sc_options['test_publish_key'] ) ? $sc_options['test_publish_key'] : '' );
+		
+		if( empty( $sc_options['test_secret_key'] ) ) {
+			$data_key = '';
+		}
 	}
 	
 	if( empty( $data_key ) ) {
