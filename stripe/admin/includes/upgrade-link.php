d3848dadb426b5cae5c2f9d3261567bb5468a285
Phil Derksen <pderksen@gmail.com>
2014-10-29 18:28:44 -0700

    Highlight text color of menu upgrade link.

--- stripe/admin/includes/upgrade-link.php
+++ stripe/admin/includes/upgrade-link.php
@@ -34,11 +34,20 @@ function sc_upgrade_link_js()
     ?>
     <script type="text/javascript">
     	jQuery(document).ready(function ($) {
+            // Open upgrade link in a new window.
             $('a[href="admin.php?page=stripe-checkout-upgrade"]').on('click', function () {
         		$(this).attr('target', '_blank');
             });
         });
     </script>
+    <style>
+        a[href="admin.php?page=stripe-checkout-upgrade"] {
+            color: #6bbc5b !important;
+        }
+        a[href="admin.php?page=stripe-checkout-upgrade"]:hover {
+            color: #7ad368 !important;
+        }
+    </style>
     <?php 
 }
 add_action( 'admin_footer', 'sc_upgrade_link_js' );
