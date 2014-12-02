90bad7d410b1d7b6f869e247f9ccc358bad2c5f5
Phil Derksen <pderksen@gmail.com>
2014-12-01 18:48:03 -0800

    Removed admin sidebar black friday promo.

--- stripe/admin/views/admin-sidebar.php
+++ stripe/admin/views/admin-sidebar.php
@@ -39,11 +39,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 					   class="button-primary button-large" target="_blank">
 						<?php _e( 'Upgrade to Pro Now', 'sc' ); ?></a>
 				</div>
-
-				<!-- Black Friday 2014 Promo -->
-				<div class="centered">
-					<h3>Use the Black Friday discount code <span style="color:red;">BF2014</span> to get 30% off any license. Expires Friday, Dec. 5.</h3>
-				</div>
 			</div>
 		</div>
 	</div>
