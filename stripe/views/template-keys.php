d46515ab4005e75050b8578d8a90fcb4a554a4f7
Nick Young <mycorpweb@gmail.com>
2015-03-26 21:14:57 -0600

    i18n some setting strings

--- stripe/views/template-keys.php
+++ stripe/views/template-keys.php
@@ -5,27 +5,27 @@
 		?>
 		
 		<div>
-			<label for="<?php echo $sc_options->get_setting_id( 'enable_live_key' ); ?>">Transaction Mode</label>
+			<label for="<?php echo $sc_options->get_setting_id( 'enable_live_key' ); ?>"><?php _e( 'Transaction Mode', 'sc' ); ?></label>
 			<?php $sc_options->toggle_control( 'enable_live_key', array( __( 'Test', 'sc' ), __( 'Live', 'sc' ) ) ); ?>
 		</div>
 		
 		<div>
-			<label for="<?php echo $sc_options->get_setting_id( 'test_secret_key' ); ?>">Test Secret Key</label>
+			<label for="<?php echo $sc_options->get_setting_id( 'test_secret_key' ); ?>"><?php _e( 'Test Secret Key', 'sc' ); ?></label>
 			<?php $sc_options->textbox( 'test_secret_key', 'regular-text' ); ?>
 		</div>
 		
 		<div>
-			<label for="<?php echo $sc_options->get_setting_id( 'test_publish_key' ); ?>">Test Publish Key</label>
+			<label for="<?php echo $sc_options->get_setting_id( 'test_publish_key' ); ?>"><?php _e( 'Test Publish Key', 'sc' ); ?></label>
 			<?php $sc_options->textbox( 'test_publish_key', 'regular-text' ); ?>
 		</div>
 		
 		<div>
-			<label for="<?php echo $sc_options->get_setting_id( 'live_secret_key' ); ?>">Live Secret Key</label>
+			<label for="<?php echo $sc_options->get_setting_id( 'live_secret_key' ); ?>"><?php _e( 'Live Secret Key', 'sc' ); ?></label>
 			<?php $sc_options->textbox( 'live_secret_key', 'regular-text' ); ?>
 		</div>
 		
 		<div>
-			<label for="<?php echo $sc_options->get_setting_id( 'live_publish_key' ); ?>">Live Publish Key</label>
+			<label for="<?php echo $sc_options->get_setting_id( 'live_publish_key' ); ?>"><?php _e( 'Live Publish Key', 'sc' ); ?></label>
 			<?php $sc_options->textbox( 'live_publish_key', 'regular-text' ); ?>
 		</div>
 
