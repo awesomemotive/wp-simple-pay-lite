7850d1456b9c6aa69a6fa192c7299881db4304d7
Nick Young <mycorpweb@gmail.com>
2015-06-05 16:08:10 -0600

    Version bump

--- stripe/classes/class-stripe-checkout.php
+++ stripe/classes/class-stripe-checkout.php
@@ -22,7 +22,7 @@ if ( ! class_exists( 'Stripe_Checkout' ) ) {
 		 *
 		 * @var     string
 		 */
-		public $version = '1.3.1';
+		public $version = '1.4.0';
 
 		/**
 		 * Unique identifier for your plugin.
