d968772777bee61d503874e1520cbf802742bf01
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2015-07-27 15:23:43 +0200

    add required php and wp versions check

---
+++ stripe/stripe-checkout-requirements.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Stripe Checkout plugin requirements
+ *
+ * Utility class to check current PHP version and WordPress to meet plugin requirements.
+ * Based on https://github.com/nekojira/wp-requirements
+ *
+ * @package SC
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	exit;
+}
+
+/**
+ * Plugin requirements.
+ *
+ * Checks for WordPress and PHP versions.
+ */
+class Stripe_Checkout_Requirements {
+
+	/**
+	 * WordPress.
+	 *
+	 * @access private
+	 * @var bool
+	 */
+	private $wp = true;
+
+	/**
+	 * PHP.
+	 *
+	 * @access private
+	 * @var bool
+	 */
+	private $php = true;
+
+	/**
+	 * Results failures.
+	 *
+	 * Associative array with requirements results.
+	 *
+	 * @access private
+	 * @var array
+	 */
+	private $failures = array();
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $requirements Associative array with requirements.
+	 */
+	public function __construct( $requirements ) {
+
+		if ( $requirements && is_array( $requirements ) ) {
+
+			$errors       = array();
+			$requirements = array_merge( array( 'wp' => '', 'php' => '' ), (array) $requirements );
+
+			// Check for WordPress version.
+			if ( $requirements['wp'] && is_string( $requirements['wp'] ) ) {
+				global $wp_version;
+				// If $wp_version isn't found or valid probably you are not running WordPress (properly)?
+				$wp_ver = $wp_version && is_string( $wp_version ) ? $wp_version : $requirements['wp'];
+				$wp_ver = version_compare( $wp_ver, $requirements['wp'] );
+				if ( $wp_ver === -1 ) {
+					$errors['wp'] = $wp_version;
+					$this->wp = false;
+				}
+			}
+
+			// Check fo PHP version.
+			if ( $requirements['php'] && is_string( $requirements['php'] ) ) {
+				$php_ver = version_compare( PHP_VERSION, $requirements['php'] );
+				if ( $php_ver === -1 ) {
+					$errors['php'] = PHP_VERSION;
+					$this->php = false;
+				}
+			}
+
+			$this->failures = $errors;
+
+		} else {
+			trigger_error( 'Stripe Checkout Requirements: the requirements requested are invalid.', E_USER_ERROR );
+		}
+	}
+
+	/**
+	 * Get requirements results.
+	 *
+	 * @return array
+	 */
+	public function failures() {
+		return $this->failures;
+	}
+
+	/**
+	 * Versions check pass.
+	 *
+	 * @return bool
+	 */
+	public function pass() {
+		return in_array( false, array( $this->wp, $this->php ) ) ? false : true;
+	}
+
+}
--- stripe/stripe-checkout.php
+++ stripe/stripe-checkout.php
@@ -1,15 +1,5 @@
 <?php
-
 /**
- * Simple Stripe Checkout
- *
- * @package   SC
- * @author    Phil Derksen <pderksen@gmail.com>, Nick Young <mycorpweb@gmail.com>
- * @license   GPL-2.0+
- * @link      http://wpstripe.net
- * @copyright 2014-2015 Phil Derksen
- *
- * @wordpress-plugin
  * Plugin Name: Simple Stripe Checkout
  * Plugin URI: http://wpstripe.net
  * Description: Add a Stripe Checkout form overlay to your site in minutes using shortcodes.
@@ -21,6 +11,12 @@
  * GitHub Plugin URI: https://github.com/pderksen/WP-Stripe-Checkout
  * Text Domain: sc
  * Domain Path: /languages/
+ *
+ * @package   SC
+ * @author    Phil Derksen <pderksen@gmail.com>, Nick Young <mycorpweb@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://wpstripe.net
+ * @copyright 2014-2015 Phil Derksen
  */
 
 // Exit if accessed directly.
@@ -28,37 +24,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! defined( 'SC_MAIN_FILE' ) ) {
-	define( 'SC_MAIN_FILE', __FILE__ );
+// Define constants.
+$stripe_checkout_requires = array( 'wp' => '3.9.0', 'php' => '5.3.0' );
+$stripe_checkout_constants = array(
+	'SC_REQUIRES'  => serialize( $stripe_checkout_requires ),
+	'SC_MAIN_FILE' => __FILE__,
+	'SC_URL'       => plugins_url( '', __FILE__ ) . '/',
+	'SC_PATH'      => plugin_dir_path( __FILE__ ),
+);
+foreach( $stripe_checkout_constants as $constant => $value ) {
+	if ( ! defined( $constant ) ) {
+		define( $constant, $value );
+	}
 }
 
-if( ! defined( 'SC_PATH' ) ) {
-	define( 'SC_PATH', plugin_dir_path( __FILE__ ) );
+// Check plugin requirements.
+include_once 'stripe-checkout-requirements.php';
+$stripe_checkout_requirements = new Stripe_Checkout_Requirements( $stripe_checkout_requires );
+if ( $stripe_checkout_requirements->pass() === false ) {
+
+	// Display an admin notice explaining why the plugin can't work.
+	function stripe_checkout_plugin_requirements() {
+		$required = unserialize( SC_REQUIRES );
+		if ( isset( $required['wp'] ) && isset( $required['php'] ) ) {
+			global $wp_version;
+			echo '<div class="error"><p>' . sprintf( __( 'Stripe Checkout requires PHP %1$s and WordPress %2$s to function properly. PHP version found: %3$s. WordPress installed version: %4$s. Please upgrade to meet the minimum requirements.', 'gce' ), $required['php'], $required['wp'], PHP_VERSION, $wp_version ) . '</p></div>';
+		}
+	}
+	add_action( 'admin_notices', 'stripe_checkout_plugin_requirements' );
+
+	$stripe_checkout_fails = $stripe_checkout_requirements->failures();
+	if ( isset( $stripe_checkout_fails['php'] ) ) {
+
+		// Deactivate the plugin
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		};
+		function stripe_checkout_plugin_deactivate_self() {
+			deactivate_plugins( plugin_basename( __FILE__ ) );
+		}
+		add_action( 'admin_init', 'stripe_checkout_plugin_deactivate_self' );
+
+		// Halt the rest of the plugin execution if PHP check fails.
+		return;
+	}
+
 }
 
-if( ! defined( 'SC_URL' ) ) {
-	define( 'SC_URL', plugins_url( '', __FILE__ ) . '/' );
-}
-
-require_once( SC_PATH . 'class-stripe-checkout.php' );
+// Load the plugin.
+require_once SC_PATH . 'class-stripe-checkout.php';
 
 // Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
 register_activation_hook( SC_MAIN_FILE, array( 'Stripe_Checkout', 'activate' ) );
 
-// Check for required and recommended versions of PHP before loading plugin.
-// https://github.com/WPupdatePHP/wp-update-php
-require_once( SC_PATH . 'libraries/WPUpdatePhp.php' );
-
-$updatePhp = new WPUpdatePhp( '5.3', '5.4' );
-$updatePhp->set_plugin_name( 'Simple Stripe Checkout' );
-
-// Show admin notice and don't execute rest of plugin if it doesn't meet required version of PHP.
-// Note the plugin will still be active.
-if ( $updatePhp->does_it_meet_required_php_version( PHP_VERSION ) ) {
-
-	// TODO Uncomment recommended admin notice once it can be hidden by user.
-	// Show admin notice for recommended version of PHP, but if required version still met continue loading plugin.
-	//$updatePhp->does_it_meet_recommended_php_version( PHP_VERSION );
-
-	Stripe_Checkout::get_instance();
-}
+Stripe_Checkout::get_instance();
