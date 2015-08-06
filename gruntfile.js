fa307e5aada1f6b22b41cea3e20dfae511ef5872
Nick Young <mycorpweb@gmail.com>
2015-08-06 13:57:03 -0600

    Updated grunfile and package; Ran release to minify files

--- gruntfile.js
+++ gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function( grunt ) {
 
 	var pkg = grunt.file.readJSON( 'package.json' ),
-		// version = 'X.Y.Z'
+	// version = 'vX.Y.Z'
 		version = pkg.version,
-		// Files to include in a release
+	// Files to include in a release
 		distFiles =  [
 			'stripe/**'
 		];
 
-	// Print current version number
-	console.log( pkg.version );
+	// Print current version number converted to semantic versioning
+	console.log( 'New version: ' + pkg.version );
 
 	// Project configuration
 	grunt.initConfig( {
@@ -41,6 +41,26 @@ module.exports = function( grunt ) {
 			}
 		},
 
+		uglify: {
+			files: {
+				expand: true,
+				cwd: 'stripe/assets/js/',
+				src: ['admin-*.js', 'public-*.js', '!*.min.js'],
+				dest: 'stripe/assets/js/',
+				ext: '.min.js'
+			}
+		},
+
+		cssmin: {
+			files: {
+				expand: true,
+				cwd: 'stripe/assets/css/',
+				src: ['admin-*.css', 'public-*.css', '!*.min.css'],
+				dest: 'stripe/assets/css/',
+				ext: '.min.css'
+			}
+		},
+
 		wp_deploy: {
 			deploy: {
 				options: {
@@ -57,7 +77,7 @@ module.exports = function( grunt ) {
 
 	// Register tasks
 
-	grunt.registerTask( 'release', ['clean', 'copy', 'compress'] );
+	grunt.registerTask( 'release', ['clean', 'uglify', 'cssmin', 'copy', 'compress'] );
 
 	grunt.registerTask( 'deploy', ['release', 'wp_deploy'] );
 
