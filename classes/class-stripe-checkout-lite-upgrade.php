4f5ab4e6c5ce0f4fea4e1ff6d6552451aaf96124
Phil Derksen <pderksen@gmail.com>
2016-02-22 12:57:48 -0800

    Code formatting.

--- classes/class-stripe-checkout-lite-upgrade.php
+++ classes/class-stripe-checkout-lite-upgrade.php
@@ -18,7 +18,7 @@ if ( ! class_exists( 'Stripe_Checkout_Upgrade' ) ) {
 		protected static $instance = null;
 		
 		/**
-		 * Class constructor
+		 * Class constructor.
 		 */
 		private function __construct() {
 			
@@ -27,8 +27,7 @@ if ( ! class_exists( 'Stripe_Checkout_Upgrade' ) ) {
 			$old_version = get_option( 'sc_version' );
 	
 			if( ! empty( $old_version ) ) {
-				
-				// Version 2.0.4 upgrade
+
 				if( version_compare( $old_version, '1.4.0', '<' ) ) {
 					add_action( 'admin_init', array( $this, 'v140_upgrade' ), 11 );
 				}
@@ -36,7 +35,6 @@ if ( ! class_exists( 'Stripe_Checkout_Upgrade' ) ) {
 
 			$new_version = $base_class->version;
 			update_option( 'sc_version', $new_version );
-
 			add_option( 'sc_upgrade_has_run', 1 );
 		}
 		
