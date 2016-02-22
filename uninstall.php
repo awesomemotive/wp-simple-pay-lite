274218ee74728272432cb394e7aed4e4c90cb46c
Phil Derksen <pderksen@gmail.com>
2016-02-22 13:19:41 -0800

    Clean up uninstall routine.

--- uninstall.php
+++ uninstall.php
@@ -11,20 +11,22 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 $settings = get_option( 'sc_settings' );
 
-if ( $settings['uninstall_save_settings'] != 1 ) {
+if ( ! isset( $settings['uninstall_save_settings'] ) || 1 != $settings['uninstall_save_settings'] ) {
 
+	// Remove options used by current version of the plugin.
 	delete_option( 'sc_settings' );
-	delete_option( 'sc_had_upgrade' );
 	delete_option( 'sc_set_defaults' );
+	delete_option( 'sc_had_upgrade' );
+	delete_option( 'sc_upgrade_has_run' );
+	delete_option( 'sc_show_admin_install_notice' );
 
-	// Also remove old plugin options
+	// Remove options that may be hanging around from old versions.
 	delete_option( 'sc_settings_master' );
 	delete_option( 'sc_settings_default' );
 	delete_option( 'sc_settings_keys' );
-	delete_option( 'sc_show_admin_install_notice' );
 	delete_option( 'sc_has_run' );
 	delete_option( 'sc_version' );
-	delete_option( 'sc_upgrade_has_run' );
 	delete_option( 'sc_settings_licenses' );
 	delete_option( 'sc_licenses' );
+	delete_option( 'sc_license' );
 }
