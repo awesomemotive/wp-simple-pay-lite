16bea0044979d840fd425b0e61a0ba625712b8c9
Phil Derksen <pderksen@gmail.com>
2016-09-26 09:38:26 -0700

    Update admin sidebar Pro upgrade info.

--- views/admin-lite-sidebar.php
+++ views/admin-lite-sidebar.php
@@ -15,20 +15,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="sidebar-container metabox-holder">
 	<div class="postbox">
-		<h3 class="wp-ui-primary"><span><?php _e( 'Need More Options?', 'stripe' ); ?></span></h3>
+		<h3 class="wp-ui-primary"><span><?php _e( 'Looking for more?', 'stripe' ); ?></span></h3>
 		<div class="inside">
 			<div class="main">
 				<p class="last-blurb centered">
-					<?php printf( __( "Additional perks you'll get with %s", 'stripe' ), Stripe_Checkout::get_pro_plugin_title() ); ?>
+					<?php printf( __( "Additional features included with %s", 'stripe' ), Stripe_Checkout::get_pro_plugin_title() ); ?>
 				</p>
 
 				<ul>
-					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Let customers enter an amount', 'stripe' ); ?></li>
-					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Collect data with custom fields', 'stripe' ); ?></li>
-					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Offer discounts with coupon codes', 'stripe' ); ?></li>
-					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Subscriptions integration', 'stripe' ); ?></li>
-					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Stripe add-ons as they\'re released', 'stripe' ); ?></li>
-					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Automatic updates & email support', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Custom Fields', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Coupon Codes', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'User-Entered Amounts', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Easy Pricing Tables Integration', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Stripe Subscriptions', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Subscription Installment Plans', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Subscription Setup Fees', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Subscription Trial Periods', 'stripe' ); ?></li>
+					<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Priority Email Support', 'stripe' ); ?></li>
 				</ul>
 
 				<div class="centered">
