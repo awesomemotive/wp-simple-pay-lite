81677c3f360b771620d5f6204277004e16945149
Nick Young <mycorpweb@gmail.com>
2016-10-13 14:26:25 -0600

    Update shared functions #27

--- classes/class-stripe-checkout-shared-functions.php
+++ classes/class-stripe-checkout-shared-functions.php
@@ -56,19 +56,13 @@ if ( ! class_exists( 'Stripe_Checkout_Functions' ) ) {
 
 			// Check first if in live or test mode.
 			if ( $sc_options->get_setting_value( 'enable_live_key' ) == 1 && $test_mode != 'true' ) {
-				if ( ! ( null === $sc_options->get_setting_value( 'live_secret_key_temp' ) ) ) {
-					$key = $sc_options->get_setting_value( 'live_secret_key_temp' );
-				} else {
-					$key = $sc_options->get_setting_value( 'live_secret_key' );
-				}
+				$key = $sc_options->get_setting_value( 'live_secret_key' );
 			} else {
-				if ( ! ( null === $sc_options->get_setting_value( 'test_secret_key_temp' ) ) ) {
-					$key = $sc_options->get_setting_value( 'test_secret_key_temp' );
-				} else {
-					$key = $sc_options->get_setting_value( 'test_secret_key' );
-				}
+				$key = $sc_options->get_setting_value( 'test_secret_key' );
 			}
-			
+
+			$key = apply_filters( 'simpay_secret_key', $key, $test_mode );
+
 			\Stripe\Stripe::setApiKey( $key );
 
 			// Send plugin name & version along with API calls.
