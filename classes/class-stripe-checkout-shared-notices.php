4052d9e914317213b9ee5e20cc18a46b5ce48a0e
Nick Young <mycorpweb@gmail.com>
2016-10-13 16:50:32 -0600

    Added admin api notice to notices class (WIP)

--- classes/class-stripe-checkout-shared-notices.php
+++ classes/class-stripe-checkout-shared-notices.php
@@ -19,9 +19,30 @@ if ( ! class_exists( 'Stripe_Checkout_Notices' ) ) {
 		 * Class constructor
 		 */
 		private function __construct() {
-			
+
 			// Add admin notice after plugin activation. Also check if should be hidden.
 			add_action( 'admin_notices', array( $this, 'admin_install_notice' ) );
+
+			// Add API notice after plugin upgrade (version 1.5.4)
+			add_action( 'admin_notices', array( $this, 'admin_api_notice' ) );
+		}
+
+		public function admin_api_notice() {
+			// Exit all of this is stored value is false/0 or not set.
+			if ( false == get_option( 'sc_show_api_notice' ) ) {
+				return;
+			}
+
+			// Delete stored value if "hide" button click detected (custom querystring value set to 1).
+			if ( ! empty( $_REQUEST['sc-dismiss-api-nag'] ) || Stripe_Checkout_Admin::get_instance()->viewing_this_plugin() ) {
+				delete_option( 'sc_show_api_notice' );
+				return;
+			}
+
+			// At this point show install notice. Show it only on the plugin screen.
+			//if( 'plugins' == get_current_screen()->id ) {
+				//include_once( SC_DIR_PATH . 'views/admin-shared-notice-install.php' );
+			//}
 		}
 		
 		/**
