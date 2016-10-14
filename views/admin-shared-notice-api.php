40d741edbc0f05cdfb8da504257d376023d77f31
Nick Young <mycorpweb@gmail.com>
2016-10-14 11:11:24 -0600

    Updated API ntice message #27

--- views/admin-shared-notice-api.php
+++ views/admin-shared-notice-api.php
@@ -22,7 +22,9 @@ global $base_class;
 <div id="sc-api-notice" class="error">
 	<p>
 		<?php
-			_e( 'This is a message to inform about API changes.', 'stripe' );
+			_e( 'This note is for WP Simple Pay plugin users that have specified multiple Stripe accounts (API keys) throughout a single WordPress site.', 'stripe' );
+			echo '<br>';
+			printf( __( 'If you are using this functionality please view the new <a target="_blank" href="%s">documentation</a> ASAP.', 'stripe' ), 'https://wpsimplepay.com/docs/misc/multiple-stripe-api-key-support/' );
 		?>
 		<a href="<?php echo esc_url( add_query_arg( 'sc-dismiss-api-nag', 1 ) ); ?>" class="button-secondary"><?php _e( 'Hide this', 'stripe' ); ?></a>
 	</p>
