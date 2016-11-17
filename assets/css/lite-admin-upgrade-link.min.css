30e3fc8d06bd2cc0e4048913128305f5637bb341
Phil Derksen <pderksen@gmail.com>
2016-11-17 09:20:41 -0800

    Minify assets.

--- assets/css/lite-admin-upgrade-link.min.css
+++ assets/css/lite-admin-upgrade-link.min.css
@@ -1,4 +1,4 @@
-/*! WP Simple Pay Lite for Stripe - 1.5.5
+/*! WP Simple Pay Lite for Stripe - 1.5.7
  * https://wordpress.org/plugins/stripe/
  * Copyright (c) Moonstone Media 2016
  * Licensed GPLv2+ */
