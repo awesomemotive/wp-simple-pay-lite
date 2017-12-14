52444012fa7d2fee9fa8cc61c03fa161b21e1312
Phil Derksen <pderksen@gmail.com>
2017-12-14 11:17:39 -0800

    Adjust version on main class __clone() & __wake() calls.

--- includes/core/main.php
+++ includes/core/main.php
@@ -70,14 +70,14 @@ final class SimplePay {
 	 * Cloning is forbidden.
 	 */
 	public function __clone() {
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'stripe' ), '2.1' );
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'stripe' ), '2.0' );
 	}
 
 	/**
 	 * Unserializing instances of this class is forbidden.
 	 */
 	public function __wakeup() {
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'stripe' ), '2.1' );
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'stripe' ), '2.0' );
 	}
 
 	/**
