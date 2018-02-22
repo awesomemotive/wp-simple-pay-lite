0cd8f7230a531942db083c37f405536d401f705c
Phil Derksen <pderksen@gmail.com>
2018-02-22 14:37:56 -0800

    Updated supported Stripe API version to 2018-01-23.

--- stripe-checkout.php
+++ stripe-checkout.php
@@ -42,7 +42,7 @@ if ( ! defined( 'SIMPLE_PAY_VERSION' ) ) {
 
 	// Stripe API version should be in 'YYYY-MM-DD' format.
 	if ( ! defined( 'SIMPLE_PAY_STRIPE_API_VERSION' ) ) {
-		define( 'SIMPLE_PAY_STRIPE_API_VERSION', '2017-12-14' );
+		define( 'SIMPLE_PAY_STRIPE_API_VERSION', '2018-01-23' );
 	}
 
 	if ( ! defined( 'SIMPLE_PAY_MAIN_FILE' ) ) {
