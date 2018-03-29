0aa5dc4586e6bdd5ac4a706ba173ecdbff66ae2a
Phil Derksen <pderksen@gmail.com>
2018-03-29 14:41:13 -0700

    More with in-plugin update message mechanism. #51

--- includes/core/main.php
+++ includes/core/main.php
@@ -71,6 +71,11 @@ final class SimplePay {
 		add_action( 'init', array( $this, 'setup_preview_form' ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ), 5 );
 
+		add_action( 'in_plugin_update_message-' . plugin_basename( SIMPLE_PAY_MAIN_FILE ), array(
+			$this,
+			'in_plugin_update_message',
+		), 10, 2 );
+
 		do_action( 'simpay_loaded' );
 	}
 
@@ -155,6 +160,93 @@ final class SimplePay {
 
 		return esc_url( apply_filters( 'simpay_get_url', $url, $case ) );
 	}
+
+	/**
+	 * Show inline plugin update message from remote readme.txt `== Upgrade Notice ==` section.
+	 * Code adapted from W3 Total Cache & WooCommerce.
+	 *
+	 * @param array  $args     Unused parameter.
+	 * @param object $response Plugin update response.
+	 */
+	public function in_plugin_update_message( $args, $response ) {
+
+		$new_version = $response->new_version;
+		$upgrade_notice = $this->get_upgrade_notice( $new_version );
+
+		echo apply_filters( 'simpay_in_plugin_update_message', $upgrade_notice ? '</p>' . wp_kses_post( $upgrade_notice ) . '<p class="dummy">' : '' ); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+	}
+
+	/**
+	 * Get the upgrade notice from hosted readme.txt file.
+	 *
+	 * @param  string $version Plugin's new version.
+	 *
+	 * @return string
+	 */
+	protected function get_upgrade_notice( $version ) {
+
+		$transient_name = 'simpay_upgrade_notice_' . $version;
+		$upgrade_notice = get_transient( $transient_name );
+
+		if ( false === $upgrade_notice ) {
+
+			// Pro readme.txt from wpsimplepay.com
+			//$response = wp_safe_remote_get( 'https://wpsimplepay.com/readmes/pro3/readme.txt' );
+
+			// Lite readme.txt from wordpress.org
+			$response = wp_safe_remote_get( 'https://plugins.svn.wordpress.org/stripe/trunk/readme.txt' );
+
+			if ( ! is_wp_error( $response ) && ! empty( $response['body'] ) ) {
+				$upgrade_notice = $this->parse_update_notice( $response['body'], $version );
+
+				// TODO set_transient( $transient_name, $upgrade_notice, HOUR_IN_SECONDS * 12 );
+
+				// Expire transient quickly for testing.
+				set_transient( $transient_name, $upgrade_notice, 1 );
+			}
+		}
+
+		return $upgrade_notice;
+	}
+
+	/**
+	 * Parse update notice from readme.txt file.
+	 *
+	 * @param  string $content readme.txt file content.
+	 * @param  string $new_version Plugin's new version.
+	 * @return string
+	 */
+	private function parse_update_notice( $content, $new_version ) {
+
+		$notice_regexp          = '~==\s*Upgrade Notice\s*==\s*=\s*(.*)\s*=(.*)(=\s*' . preg_quote( $new_version ) . '\s*=|$)~Uis';
+		$upgrade_notice         = '';
+		$upgrade_notice_version = '';
+
+		if ( version_compare( SIMPLE_PAY_VERSION, $new_version, '>' ) ) {
+			return '';
+		}
+
+		$matches = null;
+
+		if ( preg_match( $notice_regexp, $content, $matches ) ) {
+			$notices = (array) preg_split( '~[\r\n]+~', trim( $matches[2] ) );
+
+			$upgrade_notice_version = trim( $matches[1] );
+
+			if ( version_compare( SIMPLE_PAY_VERSION, $upgrade_notice_version, '<' ) ) {
+				$upgrade_notice .= '<p class="simpay_plugin_upgrade_notice">';
+
+				foreach ( $notices as $index => $line ) {
+					$upgrade_notice .= preg_replace( '~\[([^\]]*)\]\(([^\)]*)\)~', '<a href="${2}">${1}</a>', $line );
+				}
+
+				$upgrade_notice .= '</p>';
+			}
+		}
+
+		return wp_kses_post( $upgrade_notice );
+	}
+
 }
 
 /**
