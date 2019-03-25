67cb33db4489a19c656cdb07d41a128bfd6d0c78
Phil Derksen <pderksen@gmail.com>
2019-03-24 17:13:49 -0700

    Update pro feature list in payment settings & settings sidebar.

--- includes/promos/views/generic-tab-promo.php
+++ includes/promos/views/generic-tab-promo.php
@@ -9,6 +9,6 @@
 	<li><div class="dashicons dashicons-yes"></div> <?php _e( 'User-Entered Amounts', 'stripe' ); ?></li>
 	<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Coupon Codes', 'stripe' ); ?></li>
 	<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Embedded & Overlay Custom Forms', 'stripe' ); ?></li>
-	<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Apple & Google Pay (coming March 2019)', 'stripe' ); ?></li>
+	<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Apple Pay & Google Pay', 'stripe' ); ?></li>
 	<li><div class="dashicons dashicons-yes"></div> <?php _e( 'Stripe Subscription Support (Business license required)', 'stripe' ); ?></li>
 </ul>
